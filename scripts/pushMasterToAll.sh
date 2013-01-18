2c51a6955b4a66cedc89e7fda4dd2188ec1e57b5
Boris Glavic <lordpretzel@gmail.com>
2013-01-18 12:03:05 -0600

    added script for pushing the master branch changes to all student remotes

---
+++ scripts/pushMasterToAll.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+BRANCHES=`git branch | grep '^[[:space:]]*m' | sed -e 's/^\( \)*m/m/g'`
+MESSAGE=${1}
+
+for branch in ${BRANCHES};
+do
+    repos=${branch:1};
+    echo "*************** ${branch} @ ${repos} *****************"
+    git pull ${repos}
+    git checkout ${branch}
+    git merge -m "${MESSAGE}" master
+    git push ${repos}
+done
