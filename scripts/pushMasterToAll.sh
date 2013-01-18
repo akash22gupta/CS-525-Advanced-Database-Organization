4c8eb172f46f3b838655a69d1f09e6c2c45aca84
Boris Glavic <lordpretzel@gmail.com>
2013-01-18 12:15:33 -0600

    changed scripts

--- scripts/pushMasterToAll.sh
+++ scripts/pushMasterToAll.sh
@@ -7,8 +7,8 @@ for branch in ${BRANCHES};
 do
     repos=${branch:1};
     echo "*************** ${branch} @ ${repos} *****************"
-    git pull ${repos}
-    git checkout ${branch}
+    git pull "${repos}"
+    git checkout "${branch}"
     git merge -m "${MESSAGE}" master
-    git push ${repos}
+    git push "${repos}"
 done
