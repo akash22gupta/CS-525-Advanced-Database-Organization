b6e0cf19ad30b02efbae5f156c94c73971ccb54e
Boris Glavic <lordpretzel@gmail.com>
2013-01-19 12:26:29 -0600

    changed storage mgr interface and added test case

--- assign1/storage_mgr.h
+++ assign1/storage_mgr.h
@@ -3,7 +3,6 @@
 
 /* module wide constants */
 #define PAGE_SIZE 4096
-#define NULL ((void *) 0)
 
 /* return code definitions */
 typedef int RC;
@@ -31,14 +30,15 @@ typedef char* SM_PageHandle;
  *                    interface                             *
  ************************************************************/
 /* manipulating page files */
+extern void initStorageManager (void);
 extern RC createPageFile (char *fileName);
 extern RC openPageFile (char *fileName, SM_FileHandle *fHandle);
-extern RC closePageFile (SM_FileHandle *handle);
+extern RC closePageFile (SM_FileHandle *fHandle);
 extern RC destroyPageFile (char *fileName);
 
 /* reading blocks from disc */
 extern RC readBlock (int pageNum, SM_FileHandle *fHandle, SM_PageHandle *memPage);
-extern RC getBlockPos (SM_FileHandle *fHandle);
+extern int getBlockPos (SM_FileHandle *fHandle);
 extern RC readFirstBlock (SM_FileHandle *fHandle, SM_PageHandle *memPage);
 extern RC readPreviousBlock (SM_FileHandle *fHandle, SM_PageHandle *memPage);
 extern RC readCurrentBlock (SM_FileHandle *fHandle, SM_PageHandle *memPage);
@@ -49,7 +49,7 @@ extern RC readLastBlock (SM_FileHandle *fHandle, SM_PageHandle *memPage);
 extern RC writeBlock (int pageNum, SM_FileHandle *fHandle, SM_PageHandle *memPage);
 extern RC writeCurrentBlock (SM_FileHandle *fHandle, SM_PageHandle *memPage);
 extern RC appendEmptyBlock (SM_FileHandle *fHandle);
-extern RC ensureCapacity (int numberOfPages, SM_FileHandle *file);
+extern RC ensureCapacity (int numberOfPages, SM_FileHandle *fHandle);
 
 /* print a message to standard out describing the error */
 extern void printError (RC error);
--- assign1/test_assign1_1.c
+++ assign1/test_assign1_1.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
 #include "storage_mgr.h"
 
 /* define boolean data type */
@@ -14,6 +17,7 @@ typedef int bool;
 static void checkErrorCode (RC rc, char *message);
 static bool testCreateOpenClose(void);
 static bool testSinglePageContent(void);
+static void myExit (int exitCode);
 
 #define CHECK_RETURN_RC(rc) \
   do { \
@@ -25,30 +29,44 @@ static bool testSinglePageContent(void);
  
 /* main function running all tests */
 int
-main (char **args)
+main (void)
 {
   int returnCode;
+  
+  initStorageManager();
 
-  checkErrorCode(testCreateOpenClose(), "creating, opening, and closing tests failed");
-  checkErrorCode(testSinglePageContent(), "reading and writing single page content failed");
+  checkErrorCode(testCreateOpenClose(), "creating, opening, and closing");
+  checkErrorCode(testSinglePageContent(), "reading and writing single page content");
 
-  exit(0);
+  myExit(0);
+}
+
+void
+myExit (int exitCode)
+{
+  unlink(TESTPF);
+  exit(exitCode);
 }
 
 /* check a return code. If it is not RC_OK then output a message, error description, and exit */
 void
 checkErrorCode (int rc, char *message)
 {
-  if (rc == RC_OK)
+  if (rc == RC_OK) {
+    printf("TEST OK: %s\n", message);
     return;
-  exit(1);
+  }
+  printf("TEST FAILED: %s\n", message);
+  myExit(1);
 }
 
 /* fail with and error message */
 #define FAIL(condition,message)			\
-  do { \
-  printf(message); \
-  return -1; \
+  do {						\
+    if (!(condition)) {				\
+      printf("%s\n", message);			\
+      return -1;				\
+    }						\
   } while(0)
 
 /* Try to create, open, and close a page file */
@@ -60,7 +78,7 @@ testCreateOpenClose(void)
   CHECK_RETURN_RC(createPageFile (TESTPF));
   
   CHECK_RETURN_RC(openPageFile (TESTPF, &fh));
-  FAIL((strcmp(fh.fileName, TESTPF) != 0), "filename incorrect");
+  FAIL((strcmp(fh.fileName, TESTPF) == 0), "filename incorrect");
   FAIL((fh.totalNumPages == 1), "expected 1 page in new file");
   FAIL((fh.curPagePos == 0), "freshly opened file's page position should be 0");
 
@@ -68,7 +86,7 @@ testCreateOpenClose(void)
   CHECK_RETURN_RC(destroyPageFile (TESTPF));
 
   // after destruction trying to open the file should cause an error
-  FAIL((openPageFile(TESTPF, &fh) == RC_OK), "opening non-existing file should return an error.");
+  FAIL((openPageFile(TESTPF, &fh) != RC_OK), "opening non-existing file should return an error.");
 
   return RC_OK;
 }
@@ -86,22 +104,26 @@ testSinglePageContent(void)
   // create a new page file
   CHECK_RETURN_RC(createPageFile (TESTPF));
   CHECK_RETURN_RC(openPageFile (TESTPF, &fh));
+  printf("created and opened file\n");
   
   // read first page into handle
   CHECK_RETURN_RC(readFirstBlock (&fh, &ph));
   // the page should be empty (zero bytes)
   for (i=0; i < PAGE_SIZE; i++)
-    FAIL((ph[i] != '\0'), "expected zero byte in first page of freshly initialized page");
-      
+    FAIL((ph[i] == 0), "expected zero byte in first page of freshly initialized page");
+  printf("first block was empty\n");
+    
   // change ph to be a string and write that one to disk
   for (i=0; i < PAGE_SIZE; i++)
     ph[i] = (i % 10) + '0';
-  CHECK_RETURN_RC(writeCurrentBlock (&fh, &ph));
+  CHECK_RETURN_RC(writeBlock (0, &fh, &ph));
+  printf("writing first block\n");
 
   // read back the page containing the string and check that it is correct
   CHECK_RETURN_RC(readFirstBlock (&fh, &ph));
   for (i=0; i < PAGE_SIZE; i++)
     FAIL((ph[i] == (i % 10) + '0'), "character in page read from disk not the one we expected.");
+  printf("reading first block\n");
 
   // destroy new page file
   CHECK_RETURN_RC(destroyPageFile (TESTPF));  
