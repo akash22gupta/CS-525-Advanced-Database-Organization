d23cef4a185093de4fee5f12e6b8749abf22667a
Boris Glavic <lordpretzel@gmail.com>
2013-04-16 19:27:50 -0500

    Revert incorrect changes
    This reverts commit c9aa5a407ab0f452fba5dc4b9d36889f8be23b07.

---
+++ assign1/storage_mgr.c
@@ -0,0 +1,261 @@
+#include "storage_mgr.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <sys/types.h> 
+#include <sys/stat.h> 
+#include <errno.h>
+#include <fcntl.h> 
+#include <unistd.h> 
+
+char *RC_message = NULL;
+
+/* management structure for page files */
+static char *buffer = NULL;
+
+#define GET_FD(fHandle) \
+  *((int *) (fHandle->mgmtInfo))
+
+#define CHECK_FD(fd,emessage) \
+  do { \
+  if (fd < 0 ) { \
+    printf( "Error opening file: %s (%s: %i)\n", strerror(errno),  __FILE__, __LINE__); \
+    RC_message = emessage; \
+    return RC_FILE_NOT_FOUND; \
+  } \
+  } while (0)
+
+#define CHECK_SEEK(seek) \
+  do { \
+  int spos; \
+  spos = (seek); \
+  if (spos < 0 ) { \
+    printf( "Error seeking in file: %s (%s: %i)\n", strerror(errno), __FILE__, __LINE__);	\
+    return RC_FILE_NOT_FOUND; \
+  } \
+  } while (0)
+
+#define CHECK_READ(readcmd) \
+  do { \
+  int bread; \
+  bread = (readcmd); \
+  if (bread < 0 ) { \
+    printf( "Error seeking in file: %s (%s: %i)\n", strerror(errno), __FILE__, __LINE__);	\
+    return RC_FILE_NOT_FOUND; \
+  } \
+  } while (0)
+
+#define CHECK_WRITE(writecmd) \
+  do { \
+  int bwrite; \
+  bwrite = (writecmd); \
+  if (bwrite < 0 ) { \
+    printf( "Error seeking in file: %s (%s: %i)\n", strerror(errno), __FILE__, __LINE__);	\
+    return RC_FILE_NOT_FOUND; \
+  } \
+  } while (0)
+
+
+void
+initStorageManager (void)
+{
+  buffer = malloc(PAGE_SIZE);
+  printf("initialized buffer for storage manager\n");
+}
+
+RC 
+createPageFile (char *fileName)
+{
+  int fd;
+
+  fd = open(fileName, O_WRONLY | O_CREAT | O_SYNC, 0644);
+  CHECK_FD(fd,"could not create file");
+
+  // create content of header page (number of total pages = 1) and write to file
+  memset((void *) buffer, 0, PAGE_SIZE);
+  *((int *) buffer) = 1;
+
+  CHECK_WRITE(write (fd, (void *) buffer, PAGE_SIZE));
+
+  // create dummy first data page and write to file
+  memset((void *) buffer, 0, PAGE_SIZE);
+  CHECK_WRITE(write(fd, (void *) buffer, PAGE_SIZE));
+
+  // close file
+  sync();
+  close(fd);
+
+  return RC_OK;
+}
+
+RC 
+openPageFile (char *fileName, SM_FileHandle *fHandle)
+{
+  int fd;
+
+  fd = open(fileName, O_RDWR | O_SYNC);
+  CHECK_FD(fd, "could not open page file");
+	    
+  fHandle->fileName = fileName;
+
+  // read header page and set handle fields
+  CHECK_READ(read(fd, (void *) buffer, PAGE_SIZE));
+
+  fHandle->totalNumPages = *((int *) buffer);
+  fHandle->curPagePos = 0;
+  fHandle->mgmtInfo = malloc(sizeof(int));
+  GET_FD(fHandle) = fd;
+
+  printf("opened file successful\n");
+
+  return RC_OK;
+}
+
+RC 
+closePageFile (SM_FileHandle *fHandle)
+{ 
+  int spos;
+
+  // write header
+  CHECK_SEEK(lseek(GET_FD(fHandle), 0, SEEK_SET));
+  CHECK_READ(read(GET_FD(fHandle), (void *) buffer, PAGE_SIZE));
+
+  *((int *) buffer) = fHandle->totalNumPages;
+  CHECK_SEEK(lseek(GET_FD(fHandle), 0, SEEK_SET));
+  CHECK_WRITE(write(GET_FD(fHandle), (void *) buffer, PAGE_SIZE));
+
+  close(GET_FD(fHandle));
+  CHECK_FD(GET_FD(fHandle), "error closing file");
+  free(fHandle->mgmtInfo);
+
+  return RC_OK;
+}
+
+RC 
+destroyPageFile (char *fileName)
+{
+  int rt;
+
+  rt = unlink(fileName);
+  CHECK_FD(rt, "could not delete file");
+
+  return RC_OK;
+}
+
+/* reading blocks from disc */
+RC 
+readBlock (int pageNum, SM_FileHandle *fHandle, SM_PageHandle memPage)
+{
+  CHECK_SEEK(lseek(GET_FD(fHandle), (PAGE_SIZE * (pageNum + 1)) - 1, SEEK_SET));
+  CHECK_READ(read(GET_FD(fHandle), (void *) memPage, PAGE_SIZE));
+  fHandle->curPagePos = pageNum + 1;
+
+  return RC_OK;
+}
+
+int
+getBlockPos (SM_FileHandle *fHandle)
+{
+  return fHandle->curPagePos;
+}
+
+RC
+readFirstBlock (SM_FileHandle *fHandle, SM_PageHandle memPage)
+{
+  CHECK_SEEK(lseek(GET_FD(fHandle), PAGE_SIZE - 1, SEEK_SET));
+  CHECK_READ(read(GET_FD(fHandle), (void *) memPage, PAGE_SIZE));
+  fHandle->curPagePos = 1;
+  
+  return RC_OK;
+}
+
+RC
+readPreviousBlock (SM_FileHandle *fHandle, SM_PageHandle memPage)
+{
+  if (fHandle->curPagePos == 0)
+    return RC_READ_NON_EXISTING_PAGE;
+
+  CHECK_SEEK(lseek(GET_FD(fHandle), -PAGE_SIZE, SEEK_CUR));
+  CHECK_READ(read(GET_FD(fHandle), (void *) memPage, PAGE_SIZE));
+  fHandle->curPagePos--;
+
+  return RC_OK;
+}
+
+RC
+readCurrentBlock (SM_FileHandle *fHandle, SM_PageHandle memPage)
+{
+  CHECK_READ(read(GET_FD(fHandle), (void *) memPage, PAGE_SIZE));
+  
+  return RC_OK;
+}
+
+RC 
+readNextBlock (SM_FileHandle *fHandle, SM_PageHandle memPage)
+{
+  CHECK_SEEK(lseek(GET_FD(fHandle), -PAGE_SIZE, SEEK_CUR));
+  CHECK_READ(read(GET_FD(fHandle), (void *) memPage, PAGE_SIZE));
+  fHandle->curPagePos++;
+
+  return RC_OK;
+}
+
+RC 
+readLastBlock (SM_FileHandle *fHandle, SM_PageHandle memPage)
+{
+  CHECK_SEEK(lseek(GET_FD(fHandle), (fHandle->totalNumPages * PAGE_SIZE) -1, SEEK_SET));
+  CHECK_READ(read(GET_FD(fHandle), (void *) memPage, PAGE_SIZE));
+  fHandle->curPagePos = fHandle->totalNumPages;
+
+  return RC_OK;
+}
+
+/* writing blocks to a page file */
+RC
+writeBlock (int pageNum, SM_FileHandle *fHandle, SM_PageHandle memPage)
+{
+  CHECK_SEEK(lseek(GET_FD(fHandle), ((pageNum + 1) * PAGE_SIZE) - 1, SEEK_SET));
+  CHECK_WRITE(write(GET_FD(fHandle), (void *) memPage, PAGE_SIZE));
+  fHandle->curPagePos = pageNum + 1;
+
+  return RC_OK;
+}
+
+RC 
+writeCurrentBlock (SM_FileHandle *fHandle, SM_PageHandle memPage)
+{
+  CHECK_WRITE(write(GET_FD(fHandle), (void *) memPage, PAGE_SIZE));
+  fHandle->curPagePos++;
+
+  return RC_OK;
+}
+
+RC 
+appendEmptyBlock (SM_FileHandle *fHandle)
+{
+  CHECK_SEEK(lseek(GET_FD(fHandle), ((fHandle->totalNumPages + 1) * PAGE_SIZE) - 1, SEEK_SET));
+  memset(buffer, 0, PAGE_SIZE);
+  CHECK_WRITE(write(GET_FD(fHandle), (void *) buffer, PAGE_SIZE));
+  fHandle->curPagePos = ++(fHandle->totalNumPages);
+
+  return RC_OK;
+}
+
+RC 
+ensureCapacity (int numberOfPages, SM_FileHandle *fHandle)
+{
+  while(fHandle->totalNumPages < numberOfPages)
+    appendEmptyBlock(fHandle);
+  return RC_OK;
+}
+
+/* print a message to standard out describing the error */
+void 
+printError (RC error)
+{
+  if (RC_message != NULL)
+    printf("EC (%i), \"%s\"\n", error, RC_message);
+  else
+    printf("EC (%i)\n", error);
+}
---
+++ assign2/buffer_mgr.c
@@ -0,0 +1,541 @@
+#include "buffer_mgr.h"
+#include "storage_mgr.h"
+
+#include <stdlib.h>
+
+
+#define RC_BM_ACCESS_PAGE_NOT_IN_MEM 100
+#define RC_BM_UNPIN_PAGE_NOT_PINNED 101
+#define RC_UNKOWN_REPLACEMENT_STRATEGY 102
+#define RC_STRATEGY_CHOSE_FIXED_PAGE_FOR_REPLACEMENT 103
+#define RC_BM_ALL_PAGES_FIXED 104
+
+typedef char PageData[PAGE_SIZE];
+
+#define NO_FRAME -1
+
+#define MAX_NUM_PAGES 100000
+
+typedef struct PoolStats {
+  int numReads;
+  int numWrites;
+  int numReadIO;
+  int numWriteIO;
+} PoolStats;
+
+typedef struct PoolData {
+  SM_FileHandle *file;
+  PageData *frames;
+  int *pageInFrame;
+  int *frameForPage;
+  int *fixCount;
+  bool *dirty;
+  void *stratData;
+  PoolStats *stats;
+} PoolData;
+
+
+
+#define CHECK(code)				\
+  do {						\
+    int rc_internal;				\
+    rc_internal = (code);			\
+    if (rc_internal != RC_OK)			\
+      return rc_internal;			\
+  } while (0)
+
+// static functions
+static RC getFrame(BM_BufferPool *bm, BM_PageHandle *page, PoolData *pd, int *frame);
+
+#define GET_FRAME(bm,page,pd,frame)		\
+  do {						\
+   int rc_internal;				\
+   rc_internal = getFrame(bm,page,pd,frame);	\
+   if (rc_internal != RC_OK)			\
+     return rc_internal;			\
+  } while (0)
+
+static RC loadPage(BM_BufferPool *bm, int frame, int pageNum);
+
+// internal generic functions for replacement strategies
+static RC initStrategyData (BM_BufferPool *const bm);
+static RC choosePageFrame (BM_BufferPool *const bm, int *frameNum);
+static RC shutdownStrategy (BM_BufferPool *const bm);
+
+// FIFO
+typedef struct IntCell
+{
+  int data;
+  struct IntCell *next;
+} IntCell;
+
+typedef struct IntList
+{
+  IntCell *head;
+  IntCell *tail;
+  int length;
+} IntList;
+
+static IntList *lappend (IntList *list, int value);
+#define NIL (IntList *) NULL
+static IntList *ldelete (IntList *list, int pos);
+#define llength(list) ((list == NIL) ? 0 : list->length)
+
+static RC FIFO_initData (BM_BufferPool *const bm);
+static RC FIFO_chooseNext (BM_BufferPool *const bm, int *frameNum);
+static RC FIFO_shutdown (BM_BufferPool *const bm);
+
+// functions
+RC 
+initBufferPool(BM_BufferPool *const bm , const char *pageFileName, const int numPages, ReplacementStrategy strategy, void *stratData)
+{
+  SM_FileHandle *h; 
+  PoolData *pd;
+  int i, rc;
+
+  h = (SM_FileHandle *) malloc(sizeof(SM_FileHandle));
+  pd = (PoolData *) malloc(sizeof(PoolData));
+
+  CHECK(openPageFile((char *) pageFileName, h));
+
+  pd->file = h;
+  pd->frames = (PageData *) malloc(numPages * sizeof(PageData));
+  pd->pageInFrame = (int *) malloc(numPages * sizeof(int));
+  pd->frameForPage = (int *) malloc(MAX_NUM_PAGES * sizeof(int));
+  pd->fixCount = (int *) malloc(numPages * sizeof(int));
+  pd->dirty = (bool *) malloc(numPages * sizeof(bool));
+  pd->stats = (PoolStats *) malloc(sizeof(PoolStats));
+
+  pd->stats->numReads = 0;
+  pd->stats->numWrites = 0;
+  pd->stats->numReadIO = 0;
+  pd->stats->numWriteIO = 0;
+
+  for (i = 0; i < numPages; i++)
+    {
+      pd->pageInFrame[i] = NO_PAGE;
+      pd->fixCount[i] = 0;
+      pd->dirty[i] = FALSE;
+    }
+
+  for (i = 0; i < MAX_NUM_PAGES; i++)
+    {
+      pd->frameForPage[i] = NO_FRAME;
+    }
+
+  bm->pageFile = (char *) pageFileName;
+  bm->strategy = strategy;
+  bm->numPages = numPages;
+  bm->mgmtData = pd;
+
+  initStrategyData(bm);
+
+  return RC_OK;
+}
+
+RC 
+shutdownBufferPool(BM_BufferPool *const bm)
+{
+  PoolData *pd;
+
+  pd = (PoolData *) bm->mgmtData;
+
+
+  // clean up bookkeeping info for strategy
+  shutdownStrategy(bm);
+
+  // flush and close page file
+  CHECK(forceFlushPool(bm));
+  CHECK(closePageFile(pd->file));
+  
+  bm->mgmtData = NULL;
+  
+  // free pool
+  free(pd->file);
+  free(pd->frames);
+  free(pd->pageInFrame);
+  free(pd->frameForPage);
+  free(pd->fixCount);
+  free(pd->dirty);
+  free(pd->stats);
+  free(pd);
+
+  return RC_OK;
+}
+
+RC 
+forceFlushPool(BM_BufferPool *const bm)
+{
+  PoolData *pd;
+  int i;
+
+  pd = (PoolData *) bm->mgmtData;
+
+  for(i = 0; i < bm->numPages; i++)
+    {
+      if (!(pd->fixCount[i]) && pd->dirty[i] == TRUE)
+	  {
+	    CHECK(writeBlock(pd->pageInFrame[i], pd->file, (SM_PageHandle) pd->frames[i]));
+	    pd->dirty[i] = FALSE;
+
+	    pd->stats->numWriteIO++;
+	  }
+    }
+
+  return RC_OK;
+}
+
+RC 
+markDirty (BM_BufferPool *const bm, BM_PageHandle *const page)
+{
+  PoolData *pd;
+  int f = 0;
+
+  pd = (PoolData *) bm->mgmtData;  
+  GET_FRAME(bm,page,pd,&f);
+  pd->dirty[f] = TRUE;
+
+  return RC_OK;
+}
+
+RC 
+unpinPage (BM_BufferPool *const bm, BM_PageHandle *const page)
+{
+  PoolData *pd;
+  int f = 0;
+
+  pd = (PoolData *) bm->mgmtData;  
+  GET_FRAME(bm,page,pd,&f);
+
+  if (--(pd->fixCount[f]) < 0)
+    {
+      pd->fixCount[f] = 0;
+      return RC_BM_UNPIN_PAGE_NOT_PINNED;
+    }
+
+  return RC_OK;
+}
+
+RC
+getFrame(BM_BufferPool *bm, BM_PageHandle *page, PoolData *pd, int *frame)
+{
+  *frame = pd->frameForPage[page->pageNum];
+
+  if (*frame == NO_FRAME)
+    return RC_BM_ACCESS_PAGE_NOT_IN_MEM;
+  return RC_OK;
+}
+
+
+RC 
+forcePage (BM_BufferPool *const bm, BM_PageHandle *const page)
+{
+  PoolData *pd;
+  int f = 0;
+
+  pd = (PoolData *) bm->mgmtData;  
+  GET_FRAME(bm,page,pd,&f);
+
+  if (!pd->fixCount[f] && pd->dirty[f] == TRUE)
+    {
+      writeBlock(page->pageNum, pd->file, (SM_PageHandle) page->data);
+      pd->dirty[f] = FALSE;
+    }
+
+  pd->stats->numWriteIO++;
+
+  return RC_OK;
+}
+
+RC 
+pinPage (BM_BufferPool *const bm, BM_PageHandle *const page, const PageNumber pageNum)
+{
+  PoolData *pd;
+  int f = 0;
+  BM_PageHandle *oldPage;
+
+  pd = (PoolData *) bm->mgmtData;  
+  page->pageNum = pageNum;
+
+  if (getFrame(bm,page,pd,&f) == RC_BM_ACCESS_PAGE_NOT_IN_MEM)
+    {
+      CHECK(choosePageFrame(bm, &f));
+      if (pd->fixCount[f] > 0)
+	return RC_STRATEGY_CHOSE_FIXED_PAGE_FOR_REPLACEMENT;
+      
+      // if frame is not empty flush if necessary and set frame for old page to empty
+      if (pd->pageInFrame[f] != NO_PAGE)
+	{
+	  oldPage = (BM_PageHandle *) malloc(sizeof(BM_PageHandle));
+	  oldPage->pageNum = pd->pageInFrame[f];
+	  oldPage->data = pd->frames[f];
+
+	  if (pd->dirty[f])
+	      CHECK(forcePage(bm, oldPage));
+	  pd->frameForPage[oldPage->pageNum] = NO_FRAME;
+	  free(oldPage);
+	}
+      CHECK(loadPage(bm, f, pageNum));
+    }
+  
+  pd->fixCount[f]++;
+  page->data = pd->frames[f];
+
+  return RC_OK;
+}
+
+RC 
+loadPage(BM_BufferPool *bm, int frame, int pageNum)
+{
+  PoolData *pd;
+  SM_PageHandle pageData;
+
+  pd = (PoolData *) bm->mgmtData;
+  pageData = (SM_PageHandle) pd->frames[frame];
+
+  CHECK(readBlock(pageNum, pd->file, pageData));
+
+  pd->pageInFrame[frame] = pageNum;
+  pd->frameForPage[pageNum] = frame;
+  pd->dirty[frame] = FALSE;
+  pd->fixCount[frame] = 0;
+  
+  pd->stats->numReadIO++;
+
+  return RC_OK;
+}
+
+PageNumber *
+getFrameContents (BM_BufferPool *const bm)
+{
+  PoolData *pd;
+
+  pd = (PoolData *) bm->mgmtData;  
+  return (PageNumber *) pd->pageInFrame;
+}
+
+bool *
+getDirtyFlags (BM_BufferPool *const bm)
+{
+  PoolData *pd;
+
+  pd = (PoolData *) bm->mgmtData;  
+  return pd->dirty;
+}
+
+int *
+getFixCounts (BM_BufferPool *const bm)
+{
+  PoolData *pd;
+
+  pd = (PoolData *) bm->mgmtData;  
+  return pd->fixCount;
+}
+
+// stats functions
+int 
+getNumReadIO (BM_BufferPool *const bm)
+{
+  PoolStats *st = ((PoolData *) bm->mgmtData)->stats;
+
+  return st->numReadIO;
+}
+
+int 
+getNumWriteIO (BM_BufferPool *const bm)
+{
+  PoolStats *st = ((PoolData *) bm->mgmtData)->stats;
+
+  return st->numWriteIO;
+}
+
+
+// internal generic functions for replacement strategies
+RC 
+initStrategyData (BM_BufferPool *const bm)
+{
+  switch(bm->strategy)
+    {
+    case RS_FIFO:
+      FIFO_initData(bm);
+      break;
+    default:
+      return RC_UNKOWN_REPLACEMENT_STRATEGY;
+    }
+
+  return RC_OK;
+}
+
+RC 
+choosePageFrame (BM_BufferPool *const bm, int *frameNum)
+{
+  switch(bm->strategy)
+    {
+    case RS_FIFO:
+      FIFO_chooseNext(bm, frameNum);
+      break;
+    default:
+      return RC_UNKOWN_REPLACEMENT_STRATEGY;
+    }
+  
+  return RC_OK;
+}
+
+RC 
+shutdownStrategy (BM_BufferPool *const bm)
+{
+  switch(bm->strategy)
+    {
+    case RS_FIFO:
+      FIFO_shutdown(bm);
+      break;
+    default:
+      return RC_UNKOWN_REPLACEMENT_STRATEGY;
+    }
+  
+  return RC_OK;
+}
+
+// FIFO
+RC 
+FIFO_initData (BM_BufferPool *const bm)
+{
+  PoolData *pd;
+
+  pd = (PoolData *) bm->mgmtData; 
+  pd->stratData = NULL;
+
+  return RC_OK;
+}
+
+RC 
+FIFO_chooseNext (BM_BufferPool *const bm, int *frameNum)
+{
+  PoolData *pd;
+  IntList *queue;
+  IntCell *lc;
+  int i;
+
+  pd = (PoolData *) bm->mgmtData; 
+  queue = (IntList *) pd->stratData;
+
+  // if there are empty buffer frames left then choose the first empty one
+  if (llength(queue) < bm->numPages)
+    {
+      for (i = 0; i < bm->numPages; i++)
+	if (pd->pageInFrame[i] == NO_PAGE)
+	  {
+	    *frameNum = i;
+	    pd->stratData = (void *) lappend(queue, i);
+	    return RC_OK;
+	  }
+    }
+  
+  // walk through list until a page is found with fixCount 0
+  for (i = 0, lc = queue->head; lc != NULL; i++, lc = lc->next)
+    {
+      int f = lc->data;
+      if (pd->fixCount[f] == 0)
+	{
+	  *frameNum = f;
+	  queue = ldelete(queue, i);
+	  pd->stratData = (void *) lappend(queue, f);
+	  return RC_OK;
+	}
+    }
+
+  return RC_BM_ALL_PAGES_FIXED;
+}
+
+RC 
+FIFO_shutdown (BM_BufferPool *const bm)
+{
+  PoolData *pd;
+  IntList *queue;
+  IntCell *lc, *f;
+
+  pd = (PoolData *) bm->mgmtData; 
+  queue = (IntList *) pd->stratData;
+  
+  if (queue == NIL)
+    return RC_OK;
+
+  for(lc = f = queue->head; lc != NULL; f = lc)
+    {
+      lc = lc -> next;
+      free(f);
+    }
+
+  free(queue);
+
+  return RC_OK;
+}
+
+// List functions
+IntList *
+lappend (IntList *list, int value)
+{
+  IntCell *newCell;
+
+  newCell = (IntCell *) malloc(sizeof(IntCell));
+  newCell->data = value;
+  newCell->next = NULL;
+
+  if (list == NIL)
+    {
+      IntList *new;
+      new = (IntList *) malloc (sizeof(IntList));
+      new->head = newCell;
+      new->tail = newCell;
+      new->length = 1;
+      return new;
+    }
+  
+  list->tail->next = newCell;
+  list->tail = newCell;
+  list->length++;
+
+  return list;
+}
+
+IntList *
+ldelete (IntList *list, int pos)
+{
+  IntCell *cur, *prev;
+  int i;
+
+  //TODO check that pos inside list
+  
+  if (pos == 0)
+    {
+      cur = list->head;
+      list->head = cur->next;
+      list->length--;
+      free(cur);
+
+      if (list->length == 1)
+	{
+	  free(list);
+	  return NIL;
+	}
+
+      return list;
+    }
+
+  prev = list->head;
+  cur = prev->next;
+  for (i = 1; i < pos; i++)
+    {
+      prev = cur;
+      cur = cur->next;
+    }
+
+  prev->next = cur->next;
+  if (cur == list->tail)
+    list->tail = cur;
+  
+  free(cur);
+
+  return list;
+}
+
